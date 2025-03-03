9618990277dbf2045679916fa4c6176eebd738bc
fmg <fmg@duck.com>
2025-03-03 18:54:32 +0000

    Daily check prev 28 days

--- src/ine_api/daily_run.py
+++ src/ine_api/daily_run.py
@@ -1,48 +1,62 @@
 import xml.etree.ElementTree as ET
 from datetime import datetime
-import yaml
+
 import pandas as pd
 import requests
 
 
-def get_metadata(response):
-    try:
-        root = ET.fromstring(response.text)
-        indicator = root.find("indicator")
-        return (
-            indicator.find("varcd").text,
-            {
-                "theme": indicator.find("theme").text,
-                "subtheme": indicator.find("subtheme").text,
-                "keywords": indicator.find("keywords").text,
-                "title": indicator.find("title").text,
-                "last_update": indicator.find("dates/last_update").text,
-                "geo_lastlevel": indicator.find("geo_lastlevel").text,
-                "last_period_available": indicator.find(
-                    "dates/last_period_available"
-                ).text,
-                "periodicity": indicator.find("periodicity").text.strip(),
-                "update_type": indicator.find("update_type").text,
-                "description": indicator.find("description").text.strip(),
-                "json_dataset": indicator.find("json/json_dataset").text.strip(),
-                "json_metainfo": indicator.find("json/json_metainfo").text.strip(),
-            },
-        )
-    except Exception:
-        return None
+def fetch_var(var_cod):
+    def fetch_data(i):
+        url = "https://www.ine.pt/ine/xml_indic.jsp?opc=1&varcd={}&lang=PT"
+        try:
+            return requests.get(url.format(str(i).zfill(7)))
+
+        except requests.RequestException:
+            return None
+
+    def get_metadata(response):
+        try:
+            root = ET.fromstring(response.text)
+            indicator = root.find("indicator")
+            return (
+                indicator.find("varcd").text,
+                {
+                    "theme": indicator.find("theme").text,
+                    "subtheme": indicator.find("subtheme").text,
+                    "keywords": indicator.find("keywords").text,
+                    "title": indicator.find("title").text,
+                    "last_update": indicator.find("dates/last_update").text,
+                    "geo_lastlevel": indicator.find("geo_lastlevel").text,
+                    "last_period_available": indicator.find(
+                        "dates/last_period_available"
+                    ).text,
+                    "periodicity": indicator.find("periodicity").text.strip(),
+                    "update_type": indicator.find("update_type").text,
+                    "description": indicator.find("description").text.strip(),
+                    "json_dataset": indicator.find("json/json_dataset").text.strip(),
+                    "json_metainfo": indicator.find("json/json_metainfo").text.strip(),
+                },
+            )
+        except Exception:
+            return None
+
+    json = get_metadata(fetch_data(var_cod))
+    if json:
+        return {
+            "var_cod": var_cod,
+            "title": json[1].get("title"),
+            "theme": json[1].get("theme"),
+            "subtheme": json[1].get("subtheme"),
+            "description": json[1].get("description"),
+            "update_type": json[1].get("update_type"),
+            "geo_lastlevel": json[1].get("geo_lastlevel"),
+            "last_update": json[1].get("last_update"),
+            "url": f"https://www.ine.pt/xportal/xmain?xpid=INE&xpgid=ine_indicadores&indOcorrCod={str(var_cod).zfill(7)}&contexto=bd&selTab=tab2",
+            "url_meta": f"https://www.ine.pt/bddXplorer/htdocs/minfo.jsp?var_cd={str(var_cod).zfill(7)}&lingua=PT",
+        }
 
 
-def fetch_data(i):
-    url = "https://www.ine.pt/ine/xml_indic.jsp?opc=1&varcd={}&lang=PT"
-    try:
-        return requests.get(url.format(str(i).zfill(7)))
-
-    except requests.RequestException:
-        return None
-
-
-def write_daily_update_to_yaml(var_cods_updated, date):
-    data = {"date": day, "vars_updated": []}
+def write_daily_update_to_yaml(daily_data, date):
     code_to_pt = {"N": "Novo", "A": "Atualizado", "D": "Disponível"}
 
     def write_md(json_entry: dict):
@@ -57,34 +71,15 @@ def write_daily_update_to_yaml(var_cods_updated, date):
 
 """
 
-    for var_cod in var_cods_updated:
-        json = get_metadata(fetch_data(var_cod))
-        if json:
-            entry = {
-                "var_cod": var_cod,
-                "title": json[1].get("title"),
-                "theme": json[1].get("theme"),
-                "subtheme": json[1].get("subtheme"),
-                "description": json[1].get("description"),
-                "update_type": json[1].get("update_type"),
-                "geo_lastlevel": json[1].get("geo_lastlevel"),
-                "last_update": json[1].get("last_update"),
-                "url": f"https://www.ine.pt/xportal/xmain?xpid=INE&xpgid=ine_indicadores&indOcorrCod={str(var_cod).zfill(7)}&contexto=bd&selTab=tab2",
-                "url_meta": f"https://www.ine.pt/bddXplorer/htdocs/minfo.jsp?var_cd={str(var_cod).zfill(7)}&lingua=PT",
-            }
-            data["vars_updated"].append(entry)
-
-    if data["vars_updated"]:
-        with open(f"docs/_ine_updates/{date}.md", "w") as file:
-            # --- around the yaml needed for jekyll
-            file.write("---\n")
-            file.write("layout: default\n")
-            file.write(f"date: {day}\n")
-            # yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
-            file.write("---\n")
-            [file.write(write_md(x)) for x in data["vars_updated"]]
-        print(f"YAML file '{date}.yaml' written successfully.")
-    print(f"Checked {date} successfully.")
+    with open(f"docs/_ine_updates/{date}.md", "w") as file:
+        # --- around the yaml needed for jekyll
+        file.write("---\n")
+        file.write("layout: default\n")
+        file.write(f"date: {day}\n")
+        # yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
+        file.write("---\n")
+        [file.write(write_md(x)) for x in daily_data]
+    print(f"md file '{date}.md' written successfully.")
 
 
 def get_indicador_df():
@@ -112,34 +107,40 @@ def get_indicador_df():
         for a in root.findall("ResultadosPesquisa")
         for x in a.findall("Resultado")
     ]
-    return pd.DataFrame.from_records(
+    df = pd.DataFrame.from_records(
         lst,
         columns=[
             "cod_smi",
             "description",
             "portal",
             "var_cod",
-            "x",
+            "date_started",
             "last_update",
         ],
     )
+    df["last_update"] = pd.to_datetime(df["last_update"], format="%d-%m-%Y")
+    df["date_started"] = pd.to_datetime(df["date_started"], format="%d-%m-%Y")
+    return df
 
 
 if __name__ == "__main__":
     try:
         df = get_indicador_df()
-    except Exception:
-        print("error getching indicator data")
+    except Exception as e:
+        print("error fetching indicator data")
+        raise e
     else:
-        for day in [datetime.today().strftime("%Y-%m-%d")]:
-        # for day in [f"2025-02-{str(day).zfill(2)}" for day in range(1, 24)]: # retro-trick
-            ddmmyyyy_day = datetime.strptime(day, "%Y-%m-%d").strftime("%d-%m-%Y")
-            df["last_update_datetime"] = pd.to_datetime(
-                df["last_update"], format="%d-%m-%Y"
-            )
+        four_weeks_ago = pd.Timestamp.now() - pd.Timedelta(days=28)
+        df_last_4_weeks = df[df["last_update"] >= four_weeks_ago]
+        updates = {}
+        for varcod in [var_cod for var_cod in df_last_4_weeks["var_cod"]]:
+            if var_data := fetch_var(varcod):
+                day = var_data["last_update"]
+                updates.setdefault(day,[]).append(var_data)
+        for day, lst in updates.items():
+            if datetime.strptime(day, "%d-%m-%Y") > four_weeks_ago:
+                write_daily_update_to_yaml(
+                    lst,
+                    date=datetime.strptime(day, "%d-%m-%Y").strftime("%Y-%m-%d"),
+                )
 
-            updated_yesterday = df.loc[df["last_update"] == ddmmyyyy_day]
-            write_daily_update_to_yaml(
-                [var_cod for var_cod in updated_yesterday["var_cod"]],
-                date=day,
-            )
